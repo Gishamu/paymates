03d43d28c0ced6021d3755a743c88458017bf223
derrimugisha <derrimugisha@gmail.com>
2022-04-15 20:06:43 +0300

    added a checker class

--- paymetApi/views.py
+++ paymetApi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class Checker(APIView):
+    def get(self, requests):
+        return Response("When you see this message that means that you have installed me successfuly")
