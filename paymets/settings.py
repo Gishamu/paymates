125eab13e87d100473fc242806f7dc46703244a4
derrimugisha <derrimugisha@gmail.com>
2022-04-15 23:43:04 +0300

    added config value

--- paymets/settings.py
+++ paymets/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
+
+# reading .env file
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -114,6 +119,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+PAYMET_API_KEY= env("PAYMET_API_KEY")
 
 
 # Static files (CSS, JavaScript, Images)
