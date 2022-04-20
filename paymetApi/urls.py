c9f9b645e06e505d64ff4006f1492d3b9a2b73cb
derrimugisha <derrimugisha@gmail.com>
2022-04-20 21:23:07 +0300

    added the end point of viewing transaction history

--- paymetApi/urls.py
+++ paymetApi/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Checker, Charge
+from .views import Checker, Charge, ViewTransaction
 
 urlpatterns = [
     path('', Checker.as_view()),
-    path('charge', Charge.as_view())
+    path('charge', Charge.as_view()),
+    path('transaction-history/<str:customerName>', ViewTransaction.as_view())
 ]
